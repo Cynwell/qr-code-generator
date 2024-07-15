9ca672302ecadab1a3ae81ab21dc8ab0acfd9b7d
Cynwell Lau <41886378+Cynwell@users.noreply.github.com>
2024-07-15 14:51:52 +0800

    Update base path, and asset prefix configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    basePath: '/qr-code-generator',
+    assetPrefix: '/qr-code-generator/',
+}
 
 module.exports = nextConfig

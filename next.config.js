80be296a174dc207b40c1408c43fe750ae96ec2f
Cynwell Lau <41886378+Cynwell@users.noreply.github.com>
2024-07-15 18:05:20 +0800

    Update nextjs.yml and append basePath and assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/qr-code-generator',
+    assetPrefix: '/qr-code-generator/',
 }
 
 module.exports = nextConfig

b1b6356994248961c34e246103fc54b7a93a6a58
Cynwell Lau <41886378+Cynwell@users.noreply.github.com>
2024-07-16 00:20:34 +0800

    chore: Update pnpm dependencies and GitHub Actions configurations

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/qr-code-generator',
-    assetPrefix: '/qr-code-generator/',
+    // basePath: '/qr-code-generator',
+    // assetPrefix: '/qr-code-generator/',
 }
 
 module.exports = nextConfig

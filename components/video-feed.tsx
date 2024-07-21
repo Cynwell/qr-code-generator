1adea1960c7c282e9d146b15805121ea0dd1a621
Cynwell Lau <41886378+Cynwell@users.noreply.github.com>
2024-07-22 01:55:28 +0800

    chore: Update video-feed.tsx to handle camera access and device enumeration

--- components/video-feed.tsx
+++ components/video-feed.tsx
@@ -1,5 +1,5 @@
 // components/video-feed.tsx
-import { useEffect, useRef, memo, useState } from "react";
+import { useEffect, useRef, memo, useState, use } from "react";
 import { Button } from "@nextui-org/button";
 
 import { decodeQR } from "@/utils/scan-qr-code";
@@ -12,24 +12,89 @@ const VideoFeed = ({ scanning, setChunks, setTotalSegments, setMetadata, setMode
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
   const [currentDeviceId, setCurrentDeviceId] = useState<string>("");
 
+  const startVideoStream = (stream: MediaStream) => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
+      videoRef.current.setAttribute("playsinline", "true"); // Required for iOS Safari
+      const onLoadedMetadata = () => {
+      const playPromise = videoRef.current?.play();
+        if (playPromise !== undefined) {
+          playPromise.then(() => {
+            console.log("Video playback started successfully");
+            // Additional actions after successful playback
+          })
+            .catch((error) => {
+              console.error("Video playback failed:", error);
+              // Additional actions on playback error
+          });
+          videoRef.current?.removeEventListener('loadedmetadata', onLoadedMetadata);
+        }
+      }
+      videoRef.current.addEventListener('loadedmetadata', onLoadedMetadata);
+    }
+  };
+
+  const stopVideoStream = () => {
+    if (videoRef.current && videoRef.current.srcObject) {
+      const tracks = (videoRef.current.srcObject as MediaStream).getTracks();
+      tracks.forEach((track) => track.stop());
+      videoRef.current.srcObject = null;
+    }
+  };
+
+  const switchCamera = () => {
+    if (devices.length > 1) {
+      const currentDeviceIndex = devices.findIndex(device => device.deviceId === currentDeviceId);
+      const nextDeviceIndex = (currentDeviceIndex + 1) % devices.length;
+      setCurrentDeviceId(devices[nextDeviceIndex].deviceId);
+    }
+  };
+
+  // Separate effect for device enumeration
   useEffect(() => {
+    if (!scanning) {
+      return;
+    }
+
     navigator.mediaDevices.enumerateDevices().then((devices) => {
-      console.table(devices);
       const videoDevices = devices.filter(device => device.kind === "videoinput");
-      console.log("Video input devices:", videoDevices);
       setDevices(videoDevices);
-      // Optionally, set the first camera found as the initial device
       if (videoDevices.length > 0 && !currentDeviceId) {
         setCurrentDeviceId(videoDevices[0].deviceId);
       }
+    }).catch((error) => {
+      console.error("Error enumerating devices:", error);
     });
   }, [currentDeviceId]);
 
-  const switchCamera = () => {
-    const currentDeviceIndex = devices.findIndex(device => device.deviceId === currentDeviceId);
-    const nextDeviceIndex = (currentDeviceIndex + 1) % devices.length;
-    setCurrentDeviceId(devices[nextDeviceIndex].deviceId);
-  };
+  // TODO: Implement a request permission function to request camera access
+  useEffect(() => {
+    const constraints = currentDeviceId === "" ? { video: { facingMode: { exact: "environment" } } } : { video: { deviceId: { exact: currentDeviceId } } };
+    if (scanning) {
+      stopVideoStream();
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          /* use the stream */
+          if (videoRef.current) {
+            const deviceId = stream.getVideoTracks()[0].getSettings().deviceId || "";
+            setCurrentDeviceId(deviceId);
+            startVideoStream(stream);
+          }
+        })
+        .catch((error) => {
+          /* handle the error */
+          console.error("Error accessing the camera:", error);
+          stopVideoStream();
+          setCurrentDeviceId("");
+        })
+    } else {
+      // Stop the video stream when scanning is disabled
+      stopVideoStream();
+      setCurrentDeviceId("");
+    }
+    console.log("Current device ID:", currentDeviceId);
+  }, [scanning, currentDeviceId]);
 
   useEffect(() => {
     if (!scanning || !currentDeviceId) return;
@@ -43,7 +108,6 @@ const VideoFeed = ({ scanning, setChunks, setTotalSegments, setMetadata, setMode
     if (!video || !canvas || !context) {
       return;
     }
-
     const scanQRCode = () => {
       if (video.readyState === video.HAVE_ENOUGH_DATA) {
         canvas.height = video.videoHeight;
@@ -77,53 +141,16 @@ const VideoFeed = ({ scanning, setChunks, setTotalSegments, setMetadata, setMode
               if (index === 0) {
                 setMetadata(result.metadata);
               }
-                // Log the updated chunks array after the state update
-              console.table(newChunks);
+              // Log the updated chunks array after the state update
+              // console.table(newChunks);
               return newChunks;
             }
           });
-
-          // setProgressValue(chunks.length === 0 ? 0 : (chunks.filter(chunk => chunk !== null).length / chunks.length) * 100);
         }
       }
       animationFrameRef.current = requestAnimationFrame(scanQRCode);
-    };
-
-    if (scanning) {
-      navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: currentDeviceId } } }).then((stream) => {
-        // I don't know what the difference between these two approaches, the error is still here, but happened in a different place:
-        // Approach 1:
-        // video.srcObject = stream;
-        // video.setAttribute("playsinline", true); // Required for iOS Safari
-        // video.play();
-        // animationFrameRef.current = requestAnimationFrame(scanQRCode);
-
-        // Approach 2:
-        if (video) {
-          video.srcObject = stream;
-          video.setAttribute("playsinline", "true"); // Required for iOS Safari
-          const playPromise = video.play();
-          if (playPromise !== undefined) {
-            playPromise.then(() => {
-              animationFrameRef.current = requestAnimationFrame(scanQRCode);
-            }).catch((error) => {
-              console.error('Video playback failed:', error);
-            });
-          }
-        }
-      });
     }
-
-    return () => {
-      if (animationFrameRef.current) {
-        cancelAnimationFrame(animationFrameRef.current);
-      }
-      if (video && video.srcObject) {
-        const tracks = (video.srcObject as MediaStream).getTracks();
-        tracks.forEach((track) => track.stop());
-        video.srcObject = null;
-      }
-    };
+    animationFrameRef.current = requestAnimationFrame(scanQRCode);
   }, [scanning, currentDeviceId, setChunks, setTotalSegments, setMetadata, setMode]);
 
   return (
--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     output: 'export',
     basePath: '/qr-code-generator',
     assetPrefix: '/qr-code-generator/',
-    trailingSlash: true,
+    // trailingSlash: true,
 }
 
 module.exports = nextConfig
